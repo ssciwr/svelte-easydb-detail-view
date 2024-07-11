c265941e3ac22062fcf917ee229ef7747c2f88f8
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-11 13:35:48 +0200

    Tailwind CSS setup

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+const tailwindcss = require("tailwindcss");
+
+const config = {
+  plugins: [
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    tailwindcss(),
+  ],
+};
+
+module.exports = config;
