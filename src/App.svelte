7160c1c1c846e550dc7549e9b741ceb38ee85f5c
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-12 15:00:21 +0200

    Add a few examples in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,9 @@
   import EasyDbDetailView from "./components/EasyDBDetailView.svelte";
 </script>
 
-<main>
+<main class="container mx-auto">
+<!-- <EasyDbDetailView uuid="6deddff8-c600-447e-8892-3359c3516a94" l10n="de-DE" /> -->
 <EasyDbDetailView uuid="859e2318-32f6-4013-8468-ef8cec0b581b" />
 <!-- <EasyDbDetailView uuid="acbc91a3-2c1f-49be-b937-9d78de838640" /> -->
-
+<!-- <EasyDbDetailView uuid="ade6b39c-86c0-40ff-98f0-a47fb8ec669d" /> -->
 </main>
