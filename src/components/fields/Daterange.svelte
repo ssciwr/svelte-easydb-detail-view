151c8668c3bfaf39add270acf1e29daf7a567eb3
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-19 11:09:16 +0200

    Collapse trivial date ranges

--- src/components/fields/Daterange.svelte
+++ src/components/fields/Daterange.svelte
@@ -1,12 +1,19 @@
 <script>
-    import { fieldData } from "../../lib/easydbHelpers";
-    import { P } from "flowbite-svelte";
-  
-    export let data;
-    export let field;
-    export let table;
+  import { fieldData } from "../../lib/easydbHelpers";
 
-    const fdata = fieldData(data, table, field);
-  </script>
-  
-  <span class="easydb-daterange-field">{fdata["from"]} - {fdata["to"]}</span>
+  export let data;
+  export let field;
+  export let table;
+
+  const fdata = fieldData(data, table, field);
+  const from = Number(fdata["from"]);
+  const to = Number(fdata["to"]);
+</script>
+
+<!-- EasyDB seems to have a shortcut to collapse date ranges to actual dates if
+start and end that coincide. We do the same here. -->
+{#if from === to}
+  <span class="easydb-daterange-field">{from}</span>
+{:else}
+  <span class="easydb-daterange-field">{from} - {to}</span>
+{/if}
