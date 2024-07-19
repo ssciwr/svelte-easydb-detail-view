b0cfd63a07a39609efef89c74ec5b5b628aac524
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-19 11:38:26 +0200

    Add linewraps to oneline l10n fields

--- src/components/fields/OnelineL10nTextField.svelte
+++ src/components/fields/OnelineL10nTextField.svelte
@@ -3,7 +3,7 @@
   import { dataLanguagesStore } from "../../lib/stores";
   import { orderLanguages } from "../../lib/l10n";
 
-  import { Table, TableBody, TableBodyRow, TableBodyCell } from "flowbite-svelte";
+  import { P, Table, TableBody, TableBodyRow, TableBodyCell } from "flowbite-svelte";
 
   export let data;
   export let field;
@@ -17,7 +17,9 @@
     {#each orderLanguages(Object.keys(fdata), $dataLanguagesStore) as lang}
       <TableBodyRow>
         <TableBodyCell>
-          <span class="easydb-oneline-l10n-text-field">{fdata[lang]}</span>
+          <P class="w-full">
+            <span class="easydb-oneline-l10n-text-field">{fdata[lang]}</span>
+          </P>
         </TableBodyCell>
         <TableBodyCell class="w-1/4">
           <span class="easydb-oneline-l10n-language-code">{lang}</span>
