d46cdfbbd756ea12348080fa46d15e1890edfee3
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-25 15:26:20 +0200

    Implement IIIF

--- src/components/splitter/UBHDIIIFMaskSplitter.svelte
+++ src/components/splitter/UBHDIIIFMaskSplitter.svelte
@@ -3,10 +3,9 @@
 
   import { P } from "flowbite-svelte";
 
-  import NotImplemented from "../utils/NotImplemented.svelte";
-
   export let data;
   export let field;
+  export let table;
   
   const options = JSON.parse(field.options);
 
@@ -17,12 +16,14 @@
     },
   };
 
+  const dtable = table in data ? data[table] : data;
+
 </script>
 
 <P>
   <span class="text-sm easydb-label">{l10n["iiif-url"][$appLanguageStore]}</span>
 </P>
 <P>
-  {options.url_prefix}/{data._system_object_id}%3A
- <NotImplemented message="How to get the asset ID here?" />
+  <!-- NB: The "asset" in the line below is a hard-coding to HeidICON that also happens upstream! -->
+  {options.url_prefix}/{data._system_object_id}%3A{dtable.asset[0]._id}
 </P>
