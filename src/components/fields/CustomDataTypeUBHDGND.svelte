8b2106229a2f12beee0ad62966d48dd83125d100
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-25 13:38:38 +0200

    Detail information for UBHD GND Plugin

--- src/components/fields/CustomDataTypeUBHDGND.svelte
+++ src/components/fields/CustomDataTypeUBHDGND.svelte
@@ -7,9 +7,26 @@
   export let field;
 
   const fdata = fieldData(data, table, field);
+
+  function extractYear(date) {
+    if (date) {
+      return date.split("-")[0];
+    }
+    return "";
+  }
+
+  function detailString(details) {
+    if (details.dateOfDeath) {
+      if (details.dateOfBirth) {
+        return ` (${extractYear(details.dateOfBirth)} - ${extractYear(details.dateOfDeath)})`;
+      }
+      return ` (? - ${extractYear(details.dateOfDeath)})`;
+    }
+    return "";
+  }
 </script>
 
 <span class="easydb-custom-ubhdgnd">
-  {fdata.conceptName}
+  {fdata.conceptName}{detailString(fdata.conceptDetails)}
   <A href={fdata.conceptURI}>GND</A>
 </span>
