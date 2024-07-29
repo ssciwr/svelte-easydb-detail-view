8cbcbc5faaf6248aa9b45ca905abc6c61ea68836
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-29 16:38:28 +0200

    Add CLI

--- src/generate.js
+++ src/generate.js
@@ -1,5 +1,6 @@
 import { writeFile } from 'fs/promises';
 import { accessInstance } from './lib/easydbData.js';
+import { Command, Flags } from '@oclif/core';
 
 // This "trick" is super-unfortunate, but Svelte Preprocessing is fully regex-based
 // and any attempts to pass stringified data that contains backslashes will result in
@@ -9,19 +10,34 @@ function escape(str) {
   return str.replaceAll("\\\"", "__ESCAPED_QUOTES__").replaceAll("\\", "__BACKSLASH__");
 }
 
-const instance = "https://heidicon.ub.uni-heidelberg.de";
-const data = await accessInstance(instance);
+class PrefetchCommand extends Command {
+  static flags = {
+    instance: Flags.string({
+      description: 'The EasyDB instance to prefetch data from',
+      required: true,
+    }),
+  }
 
-// Inject the data into the Svelte component as a constant
-const content =
-  `export const pregen_instance = '${instance}';\n` +
-  `export const pregen_masks = JSON.parse('${escape(JSON.stringify(data.masks))}'.replaceAll("__ESCAPED_QUOTES__", "\\\\\\"").replaceAll("__BACKSLASH__", "\\\\"));\n` +
-  `export const pregen_l10n = JSON.parse('${escape(JSON.stringify(data.l10n))}'.replaceAll("__ESCAPED_QUOTES__", "\\\\\\"").replaceAll("__BACKSLASH__", "\\\\"));\n` +
-  `export const pregen_schemas = JSON.parse('${escape(JSON.stringify(data.schemas))}'.replaceAll("__ESCAPED_QUOTES__", "\\\\\\"").replaceAll("__BACKSLASH__", "\\\\"));\n`;
+  async run() {
+    const { flags } = await this.parse(PrefetchCommand);
+    const instance = flags.instance;
 
-try {
-  await writeFile('src/lib/easydbPregen.js', content);
-  console.log('Successfully regenerated the EasyDB data.');
-} catch (error) {
-  console.error('Error writing the EasyDB data:', error);
+    const data = await accessInstance(instance);
+
+    // Inject the data into the Svelte component as a constant
+    const content =
+      `export const pregen_instance = '${instance}';\n` +
+      `export const pregen_masks = JSON.parse('${escape(JSON.stringify(data.masks))}'.replaceAll("__ESCAPED_QUOTES__", "\\\\\\"").replaceAll("__BACKSLASH__", "\\\\"));\n` +
+      `export const pregen_l10n = JSON.parse('${escape(JSON.stringify(data.l10n))}'.replaceAll("__ESCAPED_QUOTES__", "\\\\\\"").replaceAll("__BACKSLASH__", "\\\\"));\n` +
+      `export const pregen_schemas = JSON.parse('${escape(JSON.stringify(data.schemas))}'.replaceAll("__ESCAPED_QUOTES__", "\\\\\\"").replaceAll("__BACKSLASH__", "\\\\"));\n`;
+
+    try {
+      await writeFile('src/lib/easydbPregen.js', content);
+      console.log('Successfully regenerated the EasyDB data.');
+    } catch (error) {
+      console.error('Error writing the EasyDB data:', error);
+    }
+  }
 }
+
+PrefetchCommand.run();
