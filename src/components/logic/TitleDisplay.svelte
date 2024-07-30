62911b0c888e866823789a336e8f58650a78a439
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-07-30 17:22:36 +0200

    Guard Breadcrumbs with if

--- src/components/logic/TitleDisplay.svelte
+++ src/components/logic/TitleDisplay.svelte
@@ -13,22 +13,26 @@
 </script>
 
 <div class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400 space-w-8 pt-6 pb-6">
-  <Breadcrumb>
-    {#each data._path.slice(0, -1) as item}
-      <BreadcrumbItem>
-        <StandardRendering data={item}/>
-      </BreadcrumbItem>
-    {/each}
-  </Breadcrumb>
+  {#if "_path" in data}
+    <Breadcrumb>
+      {#each data._path.slice(0, -1) as item}
+        <BreadcrumbItem>
+          <StandardRendering data={item}/>
+        </BreadcrumbItem>
+      {/each}
+    </Breadcrumb>
+  {/if}
   <div class="pt-4 pb-4">
     <StandardRendering data={data}/>
     {$easydbInstanceDataStore.l10n[`schema.${table}.name`][$appLanguageStore]} #{data._system_object_id}
   </div>
-  <Breadcrumb>
-    {#each data[table]._pool._path.slice(1) as item}
-      <BreadcrumbItem>
-        {bestLanguage(item.pool.name, $dataLanguagesStore)}
-      </BreadcrumbItem>
-    {/each}
-  </Breadcrumb>
+  {#if "_pool" in data[table]}
+    <Breadcrumb>
+      {#each data[table]._pool._path.slice(1) as item}
+        <BreadcrumbItem>
+          {bestLanguage(item.pool.name, $dataLanguagesStore)}
+        </BreadcrumbItem>
+      {/each}
+    </Breadcrumb>
+  {/if}
 </div>
