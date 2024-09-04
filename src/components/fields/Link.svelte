69b07edcae2a7bc9663e423109de05905e06c9f8
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-09-04 17:02:41 +0200

    Disable path rendering

--- src/components/fields/Link.svelte
+++ src/components/fields/Link.svelte
@@ -35,6 +35,13 @@
   }
 
   function hasNontrivialPath() {
+    // I disable this feature for now, because I have not found out under what circumstances
+    // exactly it is supposed to be shown and when not. An example of where the info is present,
+    // but not shown is https://heidicon.ub.uni-heidelberg.de/#/detail/23823812, Field
+    // "Thema/Bildinhalt (normiert)". Gegenbeispiel ist in DMMP https://dmmp.ub.uni-heidelberg.de/#/detail/6493
+    // Field Archiv. Once we find out, just implement the logic and remove the following line.
+    return false;
+
     if (!("_path" in fdata)) {
       return false;
     }
