7c40c593c7b4f48d86bf02b1f729f06ea2aa16bd
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-10-07 18:01:31 +0200

    Add a shim implementation for detail-linked-mask-splitter

--- src/components/splitter/splitterMapping.js
+++ src/components/splitter/splitterMapping.js
@@ -1,15 +1,18 @@
 import ChronologyConverterMaskSplitter from "./ChronologyConverterMaskSplitter.svelte";
+import DetailLinkedSplitter from "./DetailLinkedSplitter.svelte";
 import UbhdIIIFMaskSplitter from "./UBHDIIIFMaskSplitter.svelte";
 import UbhdObjMaskSplitter from "./UBHDObjMaskSplitter.svelte";
 
 export const componentMapping = {
   "chronology-converter-mask-splitter": ChronologyConverterMaskSplitter,
+  "detail-linked-mask-splitter": DetailLinkedSplitter,
   "ubhd-obj-mask-splitter": UbhdObjMaskSplitter,
   "ubhd-iiif-mask-splitter": UbhdIIIFMaskSplitter,
 };
 
 export const requiresEndMapping = {
   "chronology-converter-mask-splitter": true,
+  "detail-linked-mask-splitter": false,
   "ubhd-obj-mask-splitter": false,
   "ubhd-iiif-mask-splitter": false,
 }
