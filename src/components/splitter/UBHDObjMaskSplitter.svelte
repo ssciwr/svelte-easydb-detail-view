3b6c66137ba63181e58d3714670c6d81d6493153
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-05-08 09:52:14 +0200

    Define properties on no-op splitter to silence warnings.

--- src/components/splitter/UBHDObjMaskSplitter.svelte
+++ src/components/splitter/UBHDObjMaskSplitter.svelte
@@ -2,6 +2,10 @@
 // This is a no-op in svelte-easydb-detail-view. The reason is that the original
 // is showing a help text that hints the user at an otherwise hard to find UI element
 // in EasyDB. In our project, we control the UI, so we do not need such workarounds.
+
+  let data;
+  let field;
+  let table;
 </script>
 
 <slot />
